5d28dd3ab93fe6bf43fc25dd1e79c199086ede6f
zinme <zmmkucsmb134@gmail.com>
2025-09-25 20:22:24 +0630

    Add project file and testcase file

---
+++ OldPhonePad/OldPhonePad/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class OldPhone
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(OldPhonePad("33#"));
+        Console.WriteLine(OldPhonePad("227*#"));
+        Console.WriteLine(OldPhonePad("4433555 555666#"));
+        Console.WriteLine(OldPhonePad("8 88777444666*664#"));
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    public static string OldPhonePad(String input)
+    {
+        if (input == null) return string.Empty;
+        var keypad = new Dictionary<char, string>()
+        {
+            { '1', "&'(" },
+            { '2', "ABC" },
+            { '3', "DEF" },
+            { '4', "GHI" },
+            { '5', "JKL" },
+            { '6', "MNO" },
+            { '7', "PQRS" },
+            { '8', "TUV" },
+            { '9', "WXYZ" },
+            { '0', " " }
+        };
+
+
+        var result = new StringBuilder();
+        char currentDigit = '\0';
+        int pressCount = 0;
+
+        void CommitPending()
+        {
+            if (pressCount > 0 && keypad.ContainsKey(currentDigit))
+            {
+                string letters = keypad[currentDigit];
+                int index = (pressCount - 1) % letters.Length;
+                result.Append(letters[index]);
+            }
+            currentDigit = '\0';
+            pressCount = 0;
+        }
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            char ch = input[i];
+            if (ch == '#')
+            {
+                CommitPending();
+                break;
+            }
+            if (ch == ' ')
+            {
+                CommitPending();
+                continue;
+            }
+            if (ch == '*')
+            {
+                if (pressCount > 0)
+                {
+                    currentDigit = '\0';
+                    pressCount = 0;
+                }
+                else if (result.Length > 0)
+                {
+                    result.Length--;
+                }
+                continue;
+            }
+            if (char.IsDigit(ch))
+            {
+                if (pressCount > 0 && ch == currentDigit)
+                {
+                    pressCount++;
+                }
+                else
+                {
+                    CommitPending();
+                    currentDigit = ch;
+                    pressCount = 1;
+                }
+                continue;
+            }
+        }
+
+        CommitPending();
+        return result.ToString();
+    }
+
+}
---
+++ OldPhonePadTest/OldPhonePadTest/Test1.cs
@@ -0,0 +1,35 @@
+﻿namespace OldPhonePadTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void Test_SingleLetter()
+        {
+            string output = OldPhone.OldPhonePad("33#");
+            Assert.AreEqual("E", output);
+        }
+
+        [TestMethod]
+        public void Test_DeleteAndLetter()
+        {
+            string output = OldPhone.OldPhonePad("227*#");
+            Assert.AreEqual("B", output);
+        }
+
+        [TestMethod]
+        public void Test_Hello()
+        {
+            string output = OldPhone.OldPhonePad("4433555 555666#");
+            Assert.AreEqual("HELLO", output);
+        }
+
+
+        [TestMethod]
+        public void Test_Challange()
+        {
+            string output = OldPhone.OldPhonePad("8 88777444666*664#");
+            Assert.AreEqual("TURING", output);
+        }
+    }
+}
